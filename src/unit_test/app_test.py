35721d9978986b3d84893c7c85332d0e85130dc1
ChristianCortes <clemenscortes95@hotmail.com>
2025-07-24 10:26:22 -0600

    test

--- src/unit_test/app_test.py
+++ src/unit_test/app_test.py
@@ -1,16 +1,21 @@
 from unittest.mock import patch, MagicMock
+import pytest
 from fastapi.testclient import TestClient
 from app import app
 
+# Aquí parcheamos joblib.load para que no intente cargar el archivo real
+
+
+@pytest.fixture(autouse=True)
+def mock_joblib_load():
+    with patch("app.load", return_value=MagicMock()) as mock_load:
+        yield mock_load
+
+
 client = TestClient(app)
 
-@patch('app.load')  # mockeamos joblib.load que está usado en app.py
-def test_predict_endpoint(mock_load):
-    # Crear un mock para el modelo con método predict
-    mock_model = MagicMock()
-    mock_model.predict.return_value = [1, 0]  # ejemplo de predicciones
-    mock_load.return_value = mock_model  # load() devolverá el mock_model
 
+def test_predict_endpoint():
     sample_input = [
         {
             "InvoiceNo": "536365",
@@ -20,21 +25,10 @@ def test_predict_endpoint(mock_load):
             "InvoiceDate": "12/1/2010 8:26",
             "UnitPrice": 2.55,
             "CustomerID": 17850.0,
-            "Country": "United Kingdom"
-        },
-        {
-            "InvoiceNo": "536365",
-            "StockCode": "71053",
-            "Description": "WHITE METAL LANTERN",
-            "Quantity": 6,
-            "InvoiceDate": "12/1/2010 8:26",
-            "UnitPrice": 3.39,
-            "CustomerID": 17850.0,
-            "Country": "France"
+            "Country": "United Kingdom",
         }
     ]
 
     response = client.post("/predict/", json=sample_input)
     assert response.status_code == 200
     assert "prediction" in response.json()
-    assert isinstance(response.json()["prediction"], list)
